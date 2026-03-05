7affa585ae117b301c1075fa49046f0628761cb2
ufunny <god5228god@gmail.com>
2026-03-05 17:52:29 +0900

    프로시저 생성중

--- procedure.sql
+++ procedure.sql
@@ -1,49 +1,55 @@
 SELECT USER
 FROM DUAL;
 
--- 로그인 프로시저
-CREATE PROCEDURE PRC_LOGIN
-( P_LOGIN_ID IN USER.LOGIN_ID%TYPE
-, P_PW  IN USER.PW%TYPE
-, P_LOGIN_NAME OUT USER.NAME%TYPE
-, P_STATUS_ID OUT USER.STATUS_ID%TYPE
-, P_ROLE_ID OUT USER.ROLE_ID%TYPE
-, P_PENALTY_EDATE OUT USER.PENALTY_EDATE%TYPE
-, P_RESULT OUT VARCHAR2
+-- 소장 도서 생성 프로시저
+-- BOOK_COPY테이블 ISBN, BOOK_STATUS_ID, BOOK_COUNT
+CREATE OR REPLACE PROCEDURE PRC_BOOK_COPY_C
+( P_ISBN    IN BOOK_INFO.ISBN%TYPE
+, P_BOOK_STATUS_ID IN BOOK_STATUS.BOOK_STATUS_ID%TYPE
+, P_BOOK_COUNT  NUMBER DEFAULT 1
 )
 IS
+    V_ISBN NUMBER;
+    
+    USER_DEFINE_ERROR1  EXCEPTION;
+    USER_DEFINE_ERROR2  EXCEPTION;
 BEGIN
-    -- 아이디 존재 여부 확인
+
+    -- ISBN 유효성 검사 (BOOK_INFO에 존재하는 ISBN인지 확인)
+        SELECT COUNT(*) INTO V_ISBN
+        FROM BOOK_INFO
+        WHERE ISBN = P_ISBN;
+        
+        IF  V_ISBN = 0 THEN
+            RAISE USER_DEFINE_ERROR1;
+        END IF;
+        
+    -- COUNT 검사
+        IF  P_BOOK_COUNT < 1 OR P_BOOK_COUNT > 99 THEN
+            RAISE USER_DEFINE_ERROR2;
+        END IF;
     
-    -- 아이디가 존재하지 않는 경우
+    -- 등록(새로등록/추가등록)
+        FOR I IN 1..P_BOOK_COUNT LOOP
+            INSERT INTO BOOK_COPY(BOOK_ID, ISBN, BOOK_STATUS_ID, CREATED_AT)
+            VALUES();
+        END LOOP;
     
-    -- 비밀번호 일치 확인
-    
-    -- 비밀번호가 틀린 경우
-    
-    -- 로그인 성공
+    -- 예외처리
+    EXCEPTION
+        WHEN USER_DEFINE_ERROR1 THEN
+             RAISE_APPLICATION_ERROR(-20001, '등록되지 않은 ISBN입니다.');
+        WHEN USER_DEFINE_ERROR1 THEN
+             RAISE_APPLICATION_ERROR(-20002, '책은 1 ~ 99권 사이로 입력해야합니다.');
     
 END;
 
+
+
 -- 대출 생성 프로시저
-
 -- 대출 수정 프로시저
-
 -- 도서 연장 프로시저
+-- 도서 반납 프로시저
 
--- 반납 프로시저
 
--- 도서 권수 추가 프로시저(동일 책이 몇 권 있는지)
-
--- 도서 상태 변경 트리거
-
--- 연체 상태 자동 전환 트리거
-
--- 이벤트 권수 함수
-
--- 대출된 권수 함수
-
--- 연체중인 도서 여부 함수
-
--- 연체일 계산 함수
 
